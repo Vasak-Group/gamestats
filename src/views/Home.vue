9f7bf1347d35b056a6be154ff9be9ff42132cf39
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-07-02 15:03:53 -0300

    Eliminar el componente HelloWorld.vue y actualizar Home.vue con una nueva sección de presentación y un enlace a la página de contacto.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,26 @@
-<script setup lang="ts">
-import HelloWorld from "@/components/HelloWorld.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div class="home"></div>
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <section
+    class="relative bg-no-repeat bg-center bg-cover flex items-center p-16"
+    style="
+      background-image: url('https://bonx-react.pages.dev/static/hero-bg1-82fc7416624f7e54a222475671a5e21d.webp');
+    "
+  >
+    <div class="px-4 z-10 flex content-center justify-center w-full">
+      <div class="text-white mt-16">
+        <h1 class="mb-6 sm:mb-10 text-shadow uppercase max-w-3xl">
+          BEST GAME PLAYING TODAY.
+        </h1>
+        <p class="text-base lg:text-md font-bold mb-6 sm:mb-10">
+          Simply text of the printing and typesetting industry.
+        </p>
+        <a
+          class="text-white inline-block bg-primary text-center font-bold group hover:opacity-80  rounded-xl p-4"
+          href="/contact-us"
+          >Play Now
+        </a>
+      </div>
+    </div>
+  </section>
 </template>
