396aa20cda4dd490df14b3529e9357808015c911
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-07-02 10:31:41 -0300

    Agregar componente WebSite.vue con estructura de layout y componentes NavBar y FooterPanel

---
+++ src/layouts/WebSite.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import FooterPanel from '@/components/FooterPanel.vue';
+import NavBar from '@/components/NavBar.vue';
+</script>
+
+<template>
+  <NavBar />
+  <main>
+    <slot />
+  </main>
+  <FooterPanel />
+</template>
