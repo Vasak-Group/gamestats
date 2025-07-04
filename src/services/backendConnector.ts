c364352994937bc3b6a9cfc7dda46a5b8e0d0f32
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-07-04 09:02:49 -0300

    Agregar servicio backendConnector para gestionar solicitudes API (GET, POST, DELETE).

---
+++ src/services/backendConnector.ts
@@ -0,0 +1,57 @@
+export const api = {
+  GET: async (url: string, token: string) => {
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_BACK_SERVICE.concat(url),
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return response.json();
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  },
+  POST: async (url: string, data: any, token: string) => {
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_BACK_SERVICE.concat(url),
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(data),
+        }
+      );
+      return response.json();
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  },
+  DELETE: async (url: string, token: string) => {
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_BACK_SERVICE.concat(url),
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return response.json();
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+};
