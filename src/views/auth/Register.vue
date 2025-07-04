4ec02654f96e7062715704899c49d942c4aa59eb
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-07-04 09:01:55 -0300

    Agregar componente RegisterForm para simplificar el formulario de registro y mejorar la estructura del código.

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { RouterLink } from "vue-router";
+import RegisterForm from "@/components/forms/RegisterForm.vue";
 </script>
 
 <template>
@@ -19,56 +19,7 @@ import { RouterLink } from "vue-router";
             </div>
           </div>
         </div>
-        <form class="form-login mt-10" action="#">
-          <div class="single-fild">
-            <input
-              class="px-6 h-14 mb-6 border-secondary-90 bg-secondary-100 hover:border-primary transition-all border-2 border-solid block rounded-md w-full focus:outline-none"
-              type="text"
-              placeholder="Name"
-            />
-          </div>
-          <div class="single-fild">
-            <input
-              class="px-6 h-14 mb-6 border-secondary-90 bg-secondary-100 hover:border-primary transition-all border-2 border-solid block rounded-md w-full focus:outline-none"
-              type="email"
-              placeholder="E-mail"
-            />
-          </div>
-          <div class="single-fild">
-            <input
-              class="px-6 h-14 mb-6 border-secondary-90 bg-secondary-100 hover:border-primary transition-all border-2 border-solid block rounded-md w-full focus:outline-none"
-              type="password"
-              placeholder="password"
-            />
-          </div>
-          <div class="single-fild">
-            <input
-              class="px-6 h-14 mb-6 border-secondary-90 bg-secondary-100 hover:border-primary transition-all border-2 border-solid block rounded-md w-full focus:outline-none"
-              type="password"
-              placeholder="Retype Password"
-            />
-          </div>
-          <div class="button text-center">
-            <button
-              type="button"
-              class="text-white font-exo inline-block text-center font-bold group hover:opacity-80 leading-11 h-12 w-32 sm:h-15 sm:w-40 sm:leading-12 rounded-22 bg-primary"
-            >
-              Login
-            </button>
-          </div>
-          <div class="account-text mt-5 text-center">
-            <p>
-              Already have account,
-              <RouterLink class="font-semibold text-yellow-400" to="/login">
-                Login here
-              </RouterLink>
-              or
-              <RouterLink class="text-yellow-400 font-semibold" to="/">
-                go to home
-              </RouterLink>
-            </p>
-          </div>
-        </form>
+        <RegisterForm />
       </div>
     </div>
   </section>
