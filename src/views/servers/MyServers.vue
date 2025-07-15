86feb7e30087af73c7373305a0a9c70170a296b2
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-07-15 15:43:17 -0300

    Agregar funcionalidad de búsqueda de servidores en MyServers.vue

--- src/views/servers/MyServers.vue
+++ src/views/servers/MyServers.vue
@@ -4,6 +4,7 @@ import AddServer from "@/components/modals/AddServer.vue";
 import { ref } from "vue";
 
 const addServerModal = ref(false);
+const filterServers = ref("");
 
 const toggleModal = () => {
   addServerModal.value = !addServerModal.value;
@@ -19,16 +20,22 @@ const toggleModal = () => {
       <span class="logo text-5xl uppercase">My Servers</span>
     </div>
   </section>
-  <div class="container mx-auto">
-    <div class="flex justify-end mb-6">
+  <div class="container mx-auto grid grid-cols-1 md:grid-cols-3">
+    <div class="flex mb-6 col-span-2"></div>
+    <div class="col-span-1">
+      <input
+        type="text"
+        placeholder="Search servers..."
+        v-model="filterServers"
+        class="px-6 h-14 w-full mb-6 border-secondary/90 bg-secondary hover:border-primary transition-all border-2 border-solid rounded-2xl focus:outline-none"
+      />
       <button
         @click="toggleModal"
-        class="px-6 h-14 bg-primary text-white rounded-2xl hover:bg-primary/80 transition-all"
+        class="px-6 h-14 bg-primary text-white rounded-2xl hover:bg-primary/80 transition-all w-full"
       >
         Add Server
       </button>
     </div>
-    
   </div>
   <AddServer v-if="addServerModal" @close="toggleModal" />
 </template>
